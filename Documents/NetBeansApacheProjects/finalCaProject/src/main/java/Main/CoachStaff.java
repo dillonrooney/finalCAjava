67cbd95c5486744dece5ba9b2565392e3f3efd81
snap-balance <2022141@student.cct.ie>
2022-05-20 20:10:31 +0100

    @overwriting

--- Documents/NetBeansApacheProjects/finalCaProject/src/main/java/Main/CoachStaff.java
+++ Documents/NetBeansApacheProjects/finalCaProject/src/main/java/Main/CoachStaff.java
@@ -13,6 +13,38 @@ public class CoachStaff extends PoolStaff{
     public CoachStaff(String firstName, String lastName, int staff_Id, String department) {
         super(firstName, lastName, staff_Id, department);
     }
+    private int licanceLevel;
+    private String clubName;
+
+    public CoachStaff(int licanceLevel, String clubName, String firstName, String lastName, int staff_Id, String department) {
+        super(firstName, lastName, staff_Id, department);
+        this.licanceLevel = licanceLevel;
+        this.clubName = clubName;
+    }
+
+    public int getLicanceLevel() {
+        return licanceLevel;
+    }
+
+    public void setLicanceLevel(int licanceLevel) {
+        this.licanceLevel = licanceLevel;
+    }
+
+    public String getClubName() {
+        return clubName;
+    }
+
+    public void setClubName(String clubName) {
+        this.clubName = clubName;
+    }
+
+    @Override
+    public void showInfo() {
+          System.out.println("Coach Licance : " + this.licanceLevel);
+           System.out.println("Club Name : " + this.clubName);
+    }
+ 
+    
     
     
 }
